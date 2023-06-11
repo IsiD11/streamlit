e5c2f709b289bb4cf6ae745e58f0c7c09ea401be
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-11 14:22:59 +0530

    Update and rename RoadAccidents-Webapp.py to Home.py

--- Home.py
+++ Home.py
@@ -26,20 +26,3 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
-
-
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
-
-progress_bar = st.sidebar.progress(0)
-status_text = st.sidebar.empty()
-last_rows = np.random.randn(1, 1)
-chart = st.line_chart(last_rows)
-

8b7f4e87965513238b04ec11fa6718c4f9d7d655
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-11 14:11:52 +0530

    Update RoadAccidents-Webapp.py

--- RoadAccidents-Webapp.py
+++ RoadAccidents-Webapp.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hello",
+    page_title="Road Accident Severity Prediction",
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Road Accidents in France ! 👋")
 
-st.sidebar.success("Select a demo above.")
+st.sidebar.success("Please select a menu.")
 
 st.markdown(
     """
@@ -43,17 +43,3 @@ status_text = st.sidebar.empty()
 last_rows = np.random.randn(1, 1)
 chart = st.line_chart(last_rows)
 
-for i in range(1, 101):
-    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-    status_text.text("%i%% Complete" % i)
-    chart.add_rows(new_rows)
-    progress_bar.progress(i)
-    last_rows = new_rows
-    time.sleep(0.05)
-
-progress_bar.empty()
-
-# Streamlit widgets automatically run the script from top to bottom. Since
-# this button is not connected to any other logic, it just causes a plain
-# rerun.
-st.button("Re-run")

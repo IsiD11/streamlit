ddbe4ea3ec9065cf90cc93a1371e9b16c0afbcc0
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-11 21:41:52 +0530

    Update 1_Datasets.py

--- pages/1_Datasets.py
+++ pages/1_Datasets.py
@@ -10,7 +10,7 @@ st.set_page_config(
 
 # Content of the page
 st.markdown(f'<b><h0 style="color:#00008B;font-size:35px;">{"Datasets used in this project :"}</h0><br><br>', unsafe_allow_html=True)
-st.markdown(f'<b><h1 style="color:#ffffff;font-size:25px;">{"https://www.data.gouv.fr/en/datasets/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2019/"}</h1><br><br>', unsafe_allow_html=True)
+st.markdown(f'<b><h1 style="color:#ffffff;font-size:25px;">{"https://www.data.gouv.fr/en/datasets/bases-de-donnees-annuelles-des-accidents-corporels-de-la-circulation-routiere-annees-de-2005-a-2019/"}</h1>', unsafe_allow_html=True)
 st.markdown(f'<b><h1 style="color:#ffffff;font-size:25px;">{"https://www.kaggle.com/ahmedlahlou/accidents-in-france-from-2005-to-2016/"}</h1><br><br>', unsafe_allow_html=True)
 
 # To set the background image of the page

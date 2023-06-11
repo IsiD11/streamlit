f1a05945c6b1c7910c1f26a9a4ac282fbde24a04
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-11 15:02:35 +0530

    Update Datasets.py

--- Pages/Datasets.py
+++ Pages/Datasets.py
@@ -1,1 +1,15 @@
+#### This page gives an overview of the datasets used for this project.
 
+import streamlit as st
+
+st.set_page_config(
+    page_title="France Road Accident Data",
+    page_icon="👋",
+)
+
+st.write("# France Road Accident Data ! 👋")
+st.markdown(
+        """
+        The dataset for this project is taken from the below sources.
+        """
+)

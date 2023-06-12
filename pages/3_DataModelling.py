2cad38b21bf2860566ae02150090c1fa011b56d4
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-12 11:47:10 +0530

    Update 3_DataModelling.py

--- pages/3_DataModelling.py
+++ pages/3_DataModelling.py
@@ -1,1 +1,34 @@
 
+### This is the data modelling subpage of streamlit webapp
+
+import streamlit as st
+
+# Set page title
+st.set_page_config(
+    page_title="France Road Accident Data Analysis - Modelling",
+    layout='wide'
+)
+
+st.markdown(f'<b><h0 style="color:#00008B;font-size:35px;">{"Data Modelling:"}</h0><br><br>', unsafe_allow_html=True)
+
+# Content of the page
+option = st.selectbox(
+    'Choose a data modelling: ',
+    ('XGBoost', 'Gradient Boost', 'Random Forest','AdaBoost'))
+
+if option == 'Accident severity over years':
+    st.image('visualizations/Accident Severity over years.png')
+
+# To set the background image of the page
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-63452.jpg?size=626&ext=jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )

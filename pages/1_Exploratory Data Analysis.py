c3e76903d282e3852b29e6ac8ec19139ef098c1b
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-15 22:26:52 +0530

    Update 1_Exploratory Data Analysis.py

--- pages/1_Exploratory Data Analysis.py
+++ pages/1_Exploratory Data Analysis.py
@@ -22,12 +22,3 @@ st.markdown(
          """,
          unsafe_allow_html=True
      )
-"""
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-  df = pd.read_csv(uploaded_file).sample(n=1000)
-  
-pr = df.profile_report()
-
-st_profile_report(pr)
-"""

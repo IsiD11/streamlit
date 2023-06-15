cbbb7b12f69e736eb4db68ed9ab4c27f5c64c65a
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-15 22:23:44 +0530

    Update 1_Exploratory Data Analysis.py

--- pages/1_Exploratory Data Analysis.py
+++ pages/1_Exploratory Data Analysis.py
@@ -4,6 +4,8 @@ import streamlit as st
 
 from streamlit_pandas_profiling import st_profile_report
 
+# Content of the page
+st.markdown(f'<b><h0 style="color:#000000;font-size:35px;">{"France Road Accidents Data Analysis & Severity Prediction !"}</h0><br>', unsafe_allow_html=True)
 
 uploaded_file = st.file_uploader("Choose a file")
 if uploaded_file is not None:

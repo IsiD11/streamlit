c8c40855cc1c7fab83661334d15c5e2e3eee1c2b
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-15 14:18:25 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -8,11 +8,11 @@ st.set_page_config(
 )
 
 # Content of the page
-#st.markdown(f'<b><h0 style="color:#ffffff;font-size:35px;">{"France Road Accidents Data Analysis & Severity Prediction !"}</h0><br><br>', unsafe_allow_html=True)
+st.markdown(f'<b><h0 style="color:#ffffff;font-size:35px;">{"France Road Accidents Data Analysis & Severity Prediction !"}</h0><br><br>', unsafe_allow_html=True)
 #st.markdown(f'<b><h1 style="color:#ffffff;font-size:25px;">{"In this project it is aimed to analyse the road accidents data in France from 2005 till 2016. The input data contains lot of information about the  accident, users and involved vehicles. The aim would be to predict the severity of the accident - whether the injured person is Slightly Injured/Not injured at all Vs Heavily Injured/Died."}</h1><br><br>', unsafe_allow_html=True)
 
 # To set the background image of the page
 st.image(
             "https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg",
-            width=400, # Manually Adjust the width of the image as per requirement
+            width=1200, # Manually Adjust the width of the image as per requirement
         )

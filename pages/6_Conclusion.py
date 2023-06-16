bb8f549dfda4f588b543078d8d0e1d722bb66b80
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-16 17:54:53 +0530

    Update 6_Conclusion.py

--- pages/6_Conclusion.py
+++ pages/6_Conclusion.py
@@ -10,6 +10,19 @@ st.set_page_config(
 # Content of the page
 st.markdown(f'<b><h0 style="color:#000000;font-size:35px;">{"Conclusion!"}</h0><br>', unsafe_allow_html=True)
 
+# To set the background image of the page
+st.markdown(
+         f"""
+         <style>
+         .stApp {{
+             background-image: url("https://img.freepik.com/free-photo/abstract-luxury-gradient-blue-background-smooth-dark-blue-with-black-vignette-studio-banner_1258-63452.jpg?size=626&ext=jpg");
+             background-attachment: fixed;
+             background-size: cover
+         }}
+         </style>
+         """,
+         unsafe_allow_html=True
+     )
 
 # To insert textual content 
 

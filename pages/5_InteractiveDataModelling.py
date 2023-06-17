382597e17c7f8718f9a8392fdb93f71808aefd84
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-17 14:47:03 +0530

    Update 5_InteractiveDataModelling.py

--- pages/5_InteractiveDataModelling.py
+++ pages/5_InteractiveDataModelling.py
@@ -5,7 +5,8 @@ import streamlit as st
 import pandas as pd
 import joblib
 import sklearn
-
+import xgboost as xgb
+from xgboost import XGBClassifier
 
 def results(model):
     st.markdown('## Accuracy')

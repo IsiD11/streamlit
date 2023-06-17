c615e26eddf5a0ec5c39ec6c8527fba2f69a16b9
DeepaNMS <62841292+DeepaNMS@users.noreply.github.com>
2023-06-17 13:47:08 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -17,9 +17,8 @@ st.markdown(f'<b><h0 style="color:#000000;font-size:35px;">{"France Road Acciden
 #        )
 
 st.image("https://upload.wikimedia.org/wikipedia/commons/2/2f/Multi_vehicle_accident_-_M4_Motorway%2C_Sydney%2C_NSW_%288076208846%29.jpg",
-            width=700 # Manually Adjust the width of the image as per requirement
+            width=900 # Manually Adjust the width of the image as per requirement
         )
-st.markdown("![Alt Text](https://media.tenor.com/tuArNck3bKwAAAAC/car-crash.gif)")
     
 # To insert textual content 
 
@@ -27,6 +26,7 @@ st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#00
 st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;">{"The objective of this project is to try to predict the severity of road accidents in France using historical data. By analyzing past records, the aim is to develop a predictive model that can estimate the severity of accidents. This project presents an ideal opportunity to encompass all stages of a comprehensive Data Science project. "}</p><br>', unsafe_allow_html=True)
 st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;">{"The initial step involves studying and implementing methods to clean the dataset. This process includes identifying and rectifying any errors, inconsistencies, or missing values present in the dataset. By ensuring the dataquality and reliability, subsequent analysis can conduct to a greater model performance accuracy."}</p><br>', unsafe_allow_html=True)
 st.markdown(f'<p align="justify" font-family: "Times New Roman" style="color:#000000;">{"The next step focuses on extracting relevant characteristics from the historical data. This involves examining the various attributes and factors associated with accidents, such as weather conditions, geographical location, and other related information. By identifying the most influential features, the objective is to develop a robust model that can effectively estimate the severity of accidents based on key features."}</p><br><br><br>', unsafe_allow_html=True)
+st.markdown("![Alt Text](https://media.tenor.com/tuArNck3bKwAAAAC/car-crash.gif)")
 st.markdown(f'<p align="right" font-family: "Times New Roman" style="color:#000000;">{"TEAM - Sidi Niare, Fan Bu, Deepa Nair M S"}</p><br>', unsafe_allow_html=True)
 st.markdown(f'<p align="right" font-family: "Times New Roman" style="color:#000000;">{"Tutor- Francesco"}</p><br>', unsafe_allow_html=True)
 
